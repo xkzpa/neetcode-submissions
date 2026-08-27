0a12149bdef6dbd9bfdc34e40df9d49f68d9ffaf
xkzpa <60895614+xkzpa@users.noreply.github.com>
2026-08-27 19:06:05 +0200

    Add: sql-sellers-with-no-sales - submission-1

---
+++ SQL/sql-sellers-with-no-sales/submission-1.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+select seller_name
+from seller
+where seller_id not in (
+    select seller_id 
+    from orders 
+    where sale_date between '2020-01-01' and '2020-12-31'
+)
+order by seller_name
