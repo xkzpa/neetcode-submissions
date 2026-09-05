5533421b2ac20648f03ff815248443214e37a87a
xkzpa <60895614+xkzpa@users.noreply.github.com>
2026-09-05 11:23:41 +0200

    Add: sql-consecutive-available-seats - submission-0

---
+++ SQL/sql-consecutive-available-seats/submission-0.sql
@@ -0,0 +1,7 @@
+SELECT 
+    DISTINCT c1.seat_id
+FROM cinema c1
+    JOIN cinema c2
+        ON (c1.seat_id = c2.seat_id + 1 OR c1.seat_id = c2.seat_id - 1)
+WHERE c1.free = 1 AND c2.free = 1
+ORDER BY 1
